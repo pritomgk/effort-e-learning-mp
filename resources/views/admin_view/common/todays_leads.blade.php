9dbf9ae60d0c946108bb2e44d477d87c9f3d2a75
pritomguha62 <pritomguha62@gmail.com>
2024-07-11 01:19:53 +0600

    todays_leads.blade.php update..

--- resources/views/admin_view/common/todays_leads.blade.php
+++ resources/views/admin_view/common/todays_leads.blade.php
@@ -31,21 +31,11 @@ Admin - Todays Leads
                             <th>Email</th>
                             <th>Whatsapp</th>
                             <th>User Code</th>
-                            {{-- <th>Director</th> --}}
-                            <th>SEO</th>
-                            <th>EO</th>
+                            <th>Parent Name</th>
+                            <th>Parent User Code</th>
                             <th>Executive</th>
-                            @if (session()->get('role_id') == 1 or session()->get('role_id') == 2)
-                            <th>CP</th>
-                            <th>Presenter</th>
-                            @endif
-                            <th>Balance</th>
-                            <th>Added By</th>
-                            @if (session()->get('role_id') == 1 or session()->get('role_id') == 2)
-                                <th>Status</th>
-                            @endif
                             <th>Joined At</th>
-                            <th>Action</th>
+                            <th>...</th>
                         </tr>
                     </thead>
                     <tbody>
@@ -64,85 +54,7 @@ Admin - Todays Leads
                                         <td><a href="mailto:{{ $todays_lead->email }}">{{ $todays_lead->email }}</a></td>
                                         <td><a href="https://wa.me/{{ $todays_lead->whatsapp }}">{{ $todays_lead->whatsapp }}</a></td>
                                         <td>{{ $todays_lead->user_code }}</td>
-                                        {{-- <td>
-                                            <select name="director_id">
-                                                @foreach ($all_directors as $director)
-                                                    @if ($todays_lead->director_id == $director->admin_id)
-                                                        <option value="{{ $director->admin_id }}">{{ $director->name }}</option>
-                                                    @endif
-                                                @endforeach
-                                                @foreach ($all_directors as $director)
-                                                    <option value="{{ $director->admin_id }}">{{ $director->name }}</option>
-                                                @endforeach
-                                                
-                                            </select>
-                                        </td> --}}
-                                        <td>
-                                            <select name="seo_id">
-                                                @foreach ($all_seos as $seo)
-                                                @if ($todays_lead->seo_id == $seo->admin_id)
-                                                    <option value="{{ $seo->admin_id }}">{{ $seo->name }}</option>
-                                                @endif
-                                                @endforeach
-                                                @foreach ($all_seos as $seo)
-                                                    <option value="{{ $seo->admin_id }}">{{ $seo->name }}</option>
-                                                @endforeach
-                                            </select>
-                                        </td>
-                                        <td>
-                                            <select name="eo_id">
-                                                @foreach ($all_eos as $eo)
-                                                @if ($todays_lead->eo_id == $eo->admin_id)
-                                                    <option value="{{ $eo->admin_id }}">{{ $eo->name }}</option>
-                                                @endif
-                                                @endforeach
-                                                @foreach ($all_eos as $eo)
-                                                    <option value="{{ $eo->admin_id }}">{{ $eo->name }}</option>
-                                                @endforeach
-                                            </select>
-                                        </td>
-                                        <td>
-                                            <select name="executive_id">
-                                                @foreach ($all_executives as $executive)
-                                                @if ($todays_lead->executive_id == $executive->admin_id)
-                                                    <option value="{{ $executive->admin_id }}">{{ $executive->name }}</option>
-                                                @endif
-                                                @endforeach
-                                                @foreach ($all_executives as $executive)
-                                                    <option value="{{ $executive->admin_id }}">{{ $executive->name }}</option>
-                                                @endforeach
-                                            </select>
-                                        </td>
-
-                                        @if (session()->get('role_id') == 1 or session()->get('role_id') == 2)
-                                        <td>
-                                            <select name="cp_id">
-                                                @foreach ($all_cps as $cp)
-                                                @if ($todays_lead->cp_id == $cp->admin_id)
-                                                    <option value="{{ $cp->admin_id }}">{{ $cp->name }}</option>
-                                                @endif
-                                                @endforeach
-                                                @foreach ($all_cps as $cp)
-                                                    <option value="{{ $cp->admin_id }}">{{ $cp->name }}</option>
-                                                @endforeach
-                                            </select>
-                                        </td>
-                                        <td>
-                                            <select name="presenter_id">
-                                                @foreach ($all_presenters as $presenter)
-                                                @if ($todays_lead->presenter_id == $presenter->admin_id)
-                                                    <option value="{{ $presenter->admin_id }}">{{ $presenter->name }}</option>
-                                                @endif
-                                                @endforeach
-                                                @foreach ($all_presenters as $presenter)
-                                                    <option value="{{ $presenter->admin_id }}">{{ $presenter->name }}</option>
-                                                @endforeach
-                                            </select>
-                                        </td>
-                                        @endif
                                         
-
-                                        <td>{{ $todays_lead->balance }}</td>
                                         <td>
                                             @foreach ($all_admins as $all_admin)
                                                 @if ($todays_lead->parent_user_code == $all_admin->user_code)
@@ -155,19 +67,10 @@ Admin - Todays Leads
                                                 @endif
                                             @endforeach
                                         </td>
-                                            @if (session()->get('role_id') == 1 or session()->get('role_id') == 2)
-                                                <td>
-                                                    <select name="status">
-                                                        @if ($todays_lead->status == 1)
-                                                            <option value="1">Active</option>
-                                                        @else
-                                                        <option value="0">Inactive</option>
-                                                        @endif
-                                                        <option value="0">Inactive</option>
-                                                        <option value="1">Active</option>
-                                                    </select>
-                                                </td>
-                                            @endif
+                                        <td>{{ $todays_lead->parent_user_code }}</td>
+                                        <td>
+                                            Error! can't found
+                                        </td>
                                         <td>{{ $todays_lead->created_at }}</td>
                                         <td>
                                             <input type="hidden" hidden name="member_id" value="{{ $todays_lead->member_id }}">
@@ -189,37 +92,6 @@ Admin - Todays Leads
         </div>
     </div>
 </div>
-
-
-                
-<script>
-    
-    // $(document).ready(function() {
-    //     $('#filterButton').click(function() {
-    //         var startTimestamp = $('#startTimestamp').val();
-    //         var endTimestamp = $('#endTimestamp').val();
-
-    //         $('#filterTableActiveMember tbody tr').each(function() {
-    //             var rowTimestamp = $(this).find('td:nth-last-child(2)').text();
-
-    //             if (isWithinRange(rowTimestamp, startTimestamp, endTimestamp)) {
-    //                 $(this).show();
-    //             } else {
-    //                 $(this).hide();
-    //             }
-    //         });
-    //     });
-
-    //     function isWithinRange(timestamp, start, end) {
-    //         var timestampDate = new Date(timestamp);
-    //         var startDate = new Date(start);
-    //         var endDate = new Date(end);
-
-    //         return (timestampDate >= startDate && timestampDate <= endDate);
-    //     }
-    // });
-
-</script>
 
 
 
